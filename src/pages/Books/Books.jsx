94d9eade1a2c004fcfd66ff0fd3197be4ad164c2
md somrat islam <islammdsomrat05@gmail.com>
2025-04-17 16:32:38 +0600

    book jsonData load react router

--- src/pages/Books/Books.jsx
+++ src/pages/Books/Books.jsx
@@ -1,20 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { data } from "react-router";
+import Book from "../Book/Book";
 
-const Books = () => {
+const Books = ({ data }) => {
   const [allBooks, setAllBooks] = useState([]);
 
-  useEffect(() => {
-    fetch("BookData.json")
-      .then((res) => res.json())
-      .then((data) => {
-        setAllBooks(data);
-      });
-  });
+  // useEffect(() => {
+  //   fetch("BookData.json")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setAllBooks(data);
+  //     });
+  // });
+
+  // const BookPromised = fetch("./BookData.json").then((res) => res.json());
 
   return (
     <div>
-      <h2>boooooooks</h2>
+      <h2 className="text-2xl text-center mt-8 mb-8 p-6">boooooooks</h2>
+      <Suspense fallback={<span>Loddding...</span>}>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7  ">
+          {data.map((singelBook) => (
+            <Book key={singelBook.bookId} singelBook={singelBook}></Book>
+          ))}
+        </div>
+      </Suspense>
     </div>
   );
 };
